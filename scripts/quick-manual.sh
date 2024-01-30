d4ee26106aded50be9b671e049121b857f9b1ca2
Jy Yuan <jyyuan@google.com>
2024-01-29 17:36:15 -0800

    Do not print path to fzf when it is available.

--- scripts/quick-manual.sh
+++ scripts/quick-manual.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash -l
 
-if command -v fzf 2>/dev/null; then
+if command -v fzf &>/dev/null; then
   page="$(whatis -r . | "$HOME/.fzf/bin/fzf" --tiebreak 'begin' | sed -E 's/^([a-zA-Z0-9@:._\[+-]+)\s+\((\w+)\)\s+-.*$/\1.\2/')"
 else
   read -p 'Read manual page: ' page
