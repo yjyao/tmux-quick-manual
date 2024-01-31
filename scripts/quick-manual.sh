027dc69a7ad311c9c267dd818072a7a047cadbee
yjyao <yjyao1112@gmail.com>
2024-01-30 16:04:02 -0800

    Sort manpages by section number.

--- scripts/quick-manual.sh
+++ scripts/quick-manual.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env -S bash -l
 
+list_manpages() {
+  whatis -r . |  # List all manpages.
+    cat -n |  # Each line looks like "linenumber manpage (section) - manpage description".
+    sort -k3.2bn,3 -k1bn,1 |  # Sort by section number; break tie by linenumber.
+    sed -E 's/^\s*[0-9]+\s*//'  # Remove linenumber at the begining of the lines.
+}
+
 if command -v fzf &>/dev/null; then
-  page="$(whatis -r . | "$HOME/.fzf/bin/fzf" --tiebreak 'begin' | sed -E 's/^([a-zA-Z0-9@:._\[+-]+)\s+\((\w+)\)\s+-.*$/\1.\2/')"
+  page="$(list_manpages |
+            "$HOME/.fzf/bin/fzf" --tiebreak 'begin' |
+            sed -E 's/^(\S+)\s+\((\w+)\)\s+.*$/\1.\2/'  # Format description lines into "manpage.section".
+                                                        # For example, "grep (1) - print lines ..." becomes "grep.1".
+         )"
 else
   read -p 'Read manual page: ' page
 fi
